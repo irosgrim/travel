001c3f0770560de895041cbe7866bb6b74ead9e4
irosgrim <irosgrim@gmail.com>
2019-05-21 08:41:45 +0200

    axios

--- src/components/HeaderFeatured.jsx
+++ src/components/HeaderFeatured.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import HeaderDescriptionCard from './HeaderDescriptionCard';
 import HeaderForm from './HeaderForm';
 import HeaderFeaturedImage from './HeaderFeaturedImage';
 
 function HeaderFeatured() {
+    useEffect(() => {
+        axios(
+            `https://www.sembo.se/d/mvc/PriceFinder/GetPriceExamples?Query=country-code%3Ase&Take=8&Duration=1&FromDate=2019-05-21&ToDate=2020-05-21&NumberOfAdults=2&NumberOfChildren=&UnmappedOnly=false&fromIatas=&SemboSunRating=&HotelOfType=`
+        ).then(response => console.log(response));
+    });
     return (
         <section className="featured-header-destination">
             <HeaderDescriptionCard />
