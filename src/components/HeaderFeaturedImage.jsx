5453b0bb43ae2b64a2ec181aa68c0d64afa0252e
irosgrim <irosgrim@gmail.com>
2019-05-22 10:41:22 +0200

    image loader

--- src/components/HeaderFeaturedImage.jsx
+++ src/components/HeaderFeaturedImage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/animations.css';
 
 function HeaderFeaturedImage({ picUrl, location }) {
+    const [imageStatus, setImageStatus] = useState('loading image...');
     return (
         <div className="bg">
-            <img src={picUrl} alt={location} />
+            {imageStatus !== 'loaded' ? (
+                <div className="image-loader">
+                    <div class="lds-roller">
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                    </div>
+                </div>
+            ) : null}
+            <img
+                src={picUrl}
+                alt={location}
+                onLoad={() => setImageStatus('loaded')}
+                onError={() => setImageStatus('failed to load image')}
+            />
         </div>
     );
 }
